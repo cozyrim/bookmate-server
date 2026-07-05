07c93f73d587af6ebe2c01084d4fc2a5aaa3c55a
Chaerim Han <96909637+cozyrim@users.noreply.github.com>
2026-07-06 03:08:42 +0900

    Send guestbook push notifications asynchronously

--- src/main/java/com/exercise/bookmateserver/BookMateServerApplication.java
+++ src/main/java/com/exercise/bookmateserver/BookMateServerApplication.java
@@ -2,7 +2,9 @@ package com.exercise.bookmateserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class BookMateServerApplication {
 
